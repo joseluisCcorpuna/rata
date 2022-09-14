f975e72be8155f513c1e2b6cb3906a6ec0f9467c
joseluis <113047351+joseluisCcorpuna@users.noreply.github.com>
2022-09-14 13:54:25 -0500

    Add files via upload

---
+++ .py
@@ -0,0 +1,16 @@
+
+numero = int(input("ingresa tu edad"))
+if numero>=18:
+   print("es mayor de edad")
+else:
+         print("eres menor de edad")
+print("ingresa tu edad")
+elif edad<=0:
+    print("tu edad no puede ser 0")
+else:
+    "Menor de edad"
+print("comienzo")
+for i in [0,1,2]:
+       print("hola ", end="")
+print()
+print("final")
